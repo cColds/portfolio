f739373d4485aa303edea64e878cd8215bac1113
cColds <103373668+cColds@users.noreply.github.com>
2023-08-03 07:53:11 -0500

    Add colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 function App() {
   return (
     <div className="app">
-      <p className="text-slate-800">Heyo, it's me.</p>
-      <button className="text-white px-4 sm:px-8 py-2 sm:py-3 bg-sky-900">
-        ...
-      </button>
+      <p className="text-accent">Heyo, it's me.</p>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-dark-blue": "var(--primary-color)",
+        background: "var(--background)",
+        accent: "var(--accent)",
+      },
+    },
   },
   plugins: [],
 };
