2e9f328dcb857f12e89fcb598327f81fdea36659
cColds <103373668+cColds@users.noreply.github.com>
2023-08-03 19:54:54 -0500

    Add wave animation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,14 @@
 function Hero() {
   return (
     <div>
-      <h1 className="text-4xl">
-        Hi, I'm <span className="font-inter-bold text-accent">Cold</span>
-      </h1>
+      <div className="flex gap-2 text-4xl">
+        <h1>
+          Hi, I'm <span className="font-inter-bold text-accent">Cold</span>
+        </h1>
+        <span className="animate-[wiggle_2s_infinite] [transform-origin:70%_70%] inline-block">
+          👋
+        </span>
+      </div>
       <h2 className="text-slate-300 font-inter-bold text-2xl">Web Developer</h2>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,18 @@ export default {
         accent: "var(--accent)",
       },
 
+      keyframes: {
+        wiggle: {
+          "0%": { transform: "rotate(0)" },
+          "10%": { transform: "rotate(-15deg)" },
+          "20%": { transform: "rotate(16deg)" },
+          "30%": { transform: "rotate(-15deg)" },
+          "40%": { transform: "rotate(12deg)" },
+          "50%": { transform: "rotate(0)" },
+          "100%": { transform: "rotate(0)" },
+        },
+      },
+
       fontFamily: {
         "inter-bold": ["var(--inter-bold)", "system-ui", "-apple-system"],
       },
