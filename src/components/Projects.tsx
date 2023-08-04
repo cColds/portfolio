56a7d91bd7ae9b8d56580549cd48274ce5353c31
cColds <103373668+cColds@users.noreply.github.com>
2023-08-04 12:27:47 -0500

    Adjust projects typography and spacing

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,13 +2,13 @@ import projectData from "../projectData";
 function Projects() {
   return (
     <section className="flex flex-col p-6" id="projects">
-      <h2 className="text-2xl mb-4">Projects</h2>
+      <h1 className="text-3xl mb-4">Projects</h1>
       <div className="flex flex-col gap-6">
         {projectData.map((project) => {
           return (
             <div key={project.id} className="flex flex-col md:flex-row gap-4">
               <div className="flex flex-col gap-4">
-                <h3 className="text-xl font-bold">{project.title}</h3>
+                <h2 className="text-xl font-inter-bold">{project.title}</h2>
                 <p className="max-w-lg">{project.description}</p>
                 <div className="flex gap-4">{project.tech.join(", ")}</div>
 
