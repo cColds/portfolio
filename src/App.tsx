b9f3216ea73b349ff77d8b360d054524e940b937
cColds <103373668+cColds@users.noreply.github.com>
2023-08-04 13:23:36 -0500

    Add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 
@@ -8,6 +11,13 @@ function App() {
         <Hero />
         <Projects />
       </main>
+
+      <footer className="w-full flex justify-center h-[50px] p-4 bg-blue-900">
+        <a href="https://github.com/cColds" className="flex gap-3 items-center">
+          <span className="text-lg">cColds</span>{" "}
+          <FontAwesomeIcon icon={faGithub} className="w-5 h-5" />
+        </a>
+      </footer>
     </div>
   );
 }
