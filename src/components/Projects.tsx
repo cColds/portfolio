3438b5fb10571f4f44cd35fe244092dfc35c705b
cColds <103373668+cColds@users.noreply.github.com>
2023-08-04 21:07:39 -0500

    Format project tech

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,18 +1,29 @@
 import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 import projectData from "../projectData";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const formatTech = (...tech: string[]) => {
+  if (tech.length === 1) return tech[0];
+  if (tech.length === 2) return `${tech[0]} and ${tech[1]}`;
+
+  const lastTech = tech.pop();
+  return `${tech.join(", ")}, and ${lastTech}`;
+};
+
 function Projects() {
   return (
     <section className="flex flex-col p-6 mb-12" id="projects">
       <h1 className="text-4xl mb-4">Projects</h1>
       <div className="flex flex-col gap-6">
         {projectData.map((project) => {
+          const tech = formatTech(...project.tech);
+
           return (
             <div key={project.id} className="flex flex-col md:flex-row gap-4">
               <div className="flex flex-col gap-4">
                 <h2 className="text-xl font-inter-bold">{project.title}</h2>
                 <p className="max-w-lg">{project.description}</p>
-                <div className="flex gap-4">{project.tech.join(", ")}</div>
+                <div className="flex gap-4">Built with {tech}</div>
 
                 <div className="flex gap-4 text-accent">
                   <a href={project.links.repo} target="_blank" title="Github">
