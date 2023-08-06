11ebe254f99837c4d193de01ae79ad7d5a60cd38
cColds <103373668+cColds@users.noreply.github.com>
2023-08-05 20:19:30 -0500

    Change logo to scroll to top

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,14 +1,14 @@
 function Nav() {
   return (
     <nav className="flex px-12 py-4 justify-between items-center">
-      <a
-        href="/"
-        className="text-xl text-black bg-accent rounded-full w-8 h-8 flex items-center justify-center"
+      <button
+        onClick={() => window.scrollTo({ left: 0, top: 0 })}
+        className="text-xl text-black bg-accent rounded-full w-8 h-8"
         aria-label="home"
         title="Logo"
       >
-        <span>C</span>
-      </a>
+        C
+      </button>
 
       <ul className="flex gap-4 justify-self-end">
         <li className="transition-colors duration-300 ease-in-out hover:text-accent">
